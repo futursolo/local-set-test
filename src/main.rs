83e80d453c231f60ccb41cf7829275cb17c372be
Kaede Hoshikawa <futursolo@icloud.com>
2022-09-04 13:33:50 +0900

    Add test code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::thread;
+use std::time::Duration;
+
+use tokio::runtime::Runtime;
+use tokio::task::LocalSet;
+use tokio::time::sleep;
+
+thread_local! {
+    static LOCAL_SET: LocalSet = LocalSet::new();
+}
+
+#[tokio::main]
+async fn main() {
+    let rt = Runtime::new().expect("failed to create runtime.");
+
+    thread::spawn(move || {
+        LOCAL_SET.with(move |local_set| {
+            local_set.block_on(&rt, async move {
+                sleep(Duration::ZERO).await;
+            });
+        });
+    });
 }
