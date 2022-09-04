7f597adfb7ebe9c1da7ad485c50593d9051d49eb
Kaede Hoshikawa <futursolo@icloud.com>
2022-09-04 14:08:45 +0900

    Test 1.20.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::thread;
 
 use tokio::sync::oneshot;
+use tokio::task::LocalSet;
+
+thread_local! {
+    static LOCAL_SET: LocalSet = LocalSet::new();
+}
 
 #[tokio::main]
 async fn main() {
+    // holds runtime thread until end of main fn.
     let (_tx, rx) = oneshot::channel::<()>();
 
     thread::spawn(move || {
@@ -12,8 +18,10 @@ async fn main() {
             .build()
             .expect("failed to create runtime.");
 
-        rt.block_on(async move {
-            let _ = rx.await;
+        LOCAL_SET.with(|local_set| {
+            local_set.block_on(&rt, async move {
+                let _ = rx.await;
+            });
         });
     });
 }
