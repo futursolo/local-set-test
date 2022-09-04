d5b2f80a27270b887f3e521f2fc55f8e39f6da62
Kaede Hoshikawa <futursolo@icloud.com>
2022-09-04 13:36:35 +0900

    Spawn a task.

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,18 @@ thread_local! {
     static LOCAL_SET: LocalSet = LocalSet::new();
 }
 
+async fn sleep_once() {
+    sleep(Duration::ZERO).await;
+}
+
 #[tokio::main]
 async fn main() {
     let rt = Runtime::new().expect("failed to create runtime.");
 
     thread::spawn(move || {
         LOCAL_SET.with(move |local_set| {
-            local_set.block_on(&rt, async move {
-                sleep(Duration::ZERO).await;
-            });
+            local_set.spawn_local(sleep_once());
+            local_set.block_on(&rt, sleep_once());
         });
     });
 }
