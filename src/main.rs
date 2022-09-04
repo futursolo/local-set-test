5d4dd4e994808a1d5b7fdc45c2cf6bbfe16bb13d
Kaede Hoshikawa <futursolo@icloud.com>
2022-09-04 14:02:32 +0900

    Remove channel as well?

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::thread;
+use std::time::Duration;
 
-use tokio::sync::mpsc;
 use tokio::task::LocalSet;
-
-type SpawnTask = Box<dyn Send + FnOnce()>;
+use tokio::time::sleep;
 
 thread_local! {
     static LOCAL_SET: LocalSet = LocalSet::new();
@@ -11,8 +10,6 @@ thread_local! {
 
 #[tokio::main]
 async fn main() {
-    let (_tx, mut rx) = mpsc::unbounded_channel::<SpawnTask>();
-
     thread::spawn(move || {
         let rt = tokio::runtime::Builder::new_current_thread()
             .enable_all()
@@ -21,9 +18,7 @@ async fn main() {
 
         LOCAL_SET.with(|local_set| {
             local_set.block_on(&rt, async move {
-                while let Some(m) = rx.recv().await {
-                    m();
-                }
+                sleep(Duration::ZERO).await;
             });
         });
     });
