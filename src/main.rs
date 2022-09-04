f8e0cbd5dbc7380c7888aa6a3505e3094202be2c
Kaede Hoshikawa <futursolo@icloud.com>
2022-09-04 13:47:06 +0900

    Remove rt.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::time::Duration;
 
-use crate::rt::Runtime;
 use futures::channel::oneshot;
 use tokio::time::timeout;
 
 mod local_worker;
 mod rt;
+use local_worker::LocalWorker;
 
 // async fn sleep_once() {
 //     sleep(Duration::ZERO).await;
@@ -13,30 +13,27 @@ mod rt;
 
 #[tokio::main]
 async fn main() {
-    let runtime = Runtime::new(2).expect("failed to create runtime.");
+    let worker = LocalWorker::new().expect("failed to create worker");
 
     let (tx1, rx1) = oneshot::channel();
     let (tx2, rx2) = oneshot::channel();
 
-    runtime.spawn_pinned(move || async move {
+    worker.spawn_pinned(move || async move {
         tx1.send(std::thread::current().id())
             .expect("failed to send!");
     });
 
-    runtime.spawn_pinned(move || async move {
+    worker.spawn_pinned(move || async move {
         tx2.send(std::thread::current().id())
             .expect("failed to send!");
     });
 
-    let result1 = timeout(Duration::from_secs(5), rx1)
+    timeout(Duration::from_secs(5), rx1)
         .await
         .expect("task timed out")
         .expect("failed to receive");
-    let result2 = timeout(Duration::from_secs(5), rx2)
+    timeout(Duration::from_secs(5), rx2)
         .await
         .expect("task timed out")
         .expect("failed to receive");
-
-    // first task and second task are not on the same thread.
-    assert_ne!(result1, result2);
 }
